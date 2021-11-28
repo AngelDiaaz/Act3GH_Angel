4ac4c0907702ec77fb976e4c78f0916b298bdbc4
adiaz <adiaz@LAPTOP-O5PN37AR>
2021-11-28 13:03:35 +0100

    Añadir contador de numeros

--- src/Metodos/NumeroAleatorios.java
+++ src/Metodos/NumeroAleatorios.java
@@ -3,12 +3,27 @@ package Metodos;
 public class NumeroAleatorios {
 
 	public static void main(String[] args) {
-		int[] array = new int [20];
+		int[] array = new int[20];
+		int num, contador;
 		
 		for (int i = 0; i < array.length; i++) {
 			array[i] = (int) (Math.random()*10);
-			System.out.print(array[i] + " ");
 		}
-	}
+		
+		for (int i = 0; i < array.length; i++) {
+			num = array[i];
+			contador = 0;
+			for (int j = 0; j < array.length; j++) {
+				if (num == array[j]) {
+					contador++;
+				}
+			}
+			if (contador == 1)
+				System.out.println(array[i] + " se repite " + contador + " vez");
+			else
+				System.out.println(array[i] + " se repite " + contador + " veces");
 
+		}
+
+}
 }
