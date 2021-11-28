850266d310320b581bb78c8401934f6c4024aff8
adiaz <adiaz@LAPTOP-O5PN37AR>
2021-11-28 13:24:14 +0100

    Modificar de 20 numeros a 30

--- src/Metodos/NumeroAleatorios.java
+++ src/Metodos/NumeroAleatorios.java
@@ -3,7 +3,7 @@ package Metodos;
 public class NumeroAleatorios {
 
 	public static void main(String[] args) {
-		int[] array = new int [20];
+		int[] array = new int [30];
 		
 		for (int i = 0; i < array.length; i++) {
 			array[i] = (int) (Math.random()*10);
